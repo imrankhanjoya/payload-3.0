ecfdd626a55bb6bae466c220c7a4a286a6123d35
bmkaradia <bmk302006@gmail.com>
2024-12-20 11:44:29 +0530

    fix 5

--- src/access/isAdmin.tsx
+++ src/access/isAdmin.tsx
@@ -4,6 +4,6 @@ export const isAdmin: Access = ({ req: { user } }) => {
   // if (user?.role?.includes('admin')) {
   //   return true; // Explicit true
   // }
-  const role = user?.role ?? []
+  const role = (user?.role ?? []) as string[] // Assert type to string[]
   return role.includes('admin')
 }
--- src/access/isAdminOrSelf.tsx
+++ src/access/isAdminOrSelf.tsx
@@ -1,9 +1,7 @@
 import { Access } from 'payload'
 
 export const isAdminOrSelf: Access = ({ req: { user } }) => {
-  const role = user?.role ?? []
-  // return role.includes('admin')
-
+  const role = (user?.role ?? []) as string[] // Assert type to string[]
   if (user) {
     if (role?.includes('admin')) {
       return true
