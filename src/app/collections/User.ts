f5ba827995915a68f2a2dc936afbe006ff0a0dd6
bmkaradia <bmk302006@gmail.com>
2024-12-30 14:20:04 +0530

    autologin

--- src/app/collections/User.ts
+++ src/app/collections/User.ts
@@ -1,6 +1,10 @@
 import type { CollectionConfig } from 'payload'
 import { isAdminOrEditor } from '@/access/isAdminOrEditor'
 import { isAdmin } from '@/access/isAdmin'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import configPromise from '@payload-config'
+import jwt from 'jsonwebtoken'
+import { cookies } from 'next/headers'
 
 export const User: CollectionConfig = {
   slug: 'users',
@@ -42,6 +46,64 @@ export const User: CollectionConfig = {
     {
       name: 'phone',
       type: 'text',
+    },
+  ],
+
+  endpoints: [
+    {
+      path: '/oauth:email',
+      method: 'get',
+      handler: async (req) => {
+        const payload = await getPayloadHMR({ config: configPromise })
+
+        const userCollection = 'users' // Replace with your auth-enabled collection slug
+        // const email = 'avi@gmail.com'
+        const { email } = req.routeParams
+
+        const userDocs = await payload.find({
+          collection: userCollection,
+          where: {
+            email: {
+              equals: email,
+            },
+          },
+          depth: 0,
+          limit: 1,
+        })
+
+        if (userDocs.totalDocs === 0) {
+          return Response.json({ error: 'User not found.' }) // res.status(400).json({ error: 'User not found.' })
+        }
+
+        const user = userDocs.docs[0]
+        // const collectionConfig = payload.collections[userCollection]?.config;
+
+        // if (!collectionConfig) {
+        //   return res.status(500).json({ error: 'Collection configuration not found.' });
+        // }
+
+        const fieldsToSign = {
+          email: user.email,
+          id: user.id,
+          collection: 'users',
+        }
+
+        const token = jwt.sign(fieldsToSign, payload.secret, {
+          expiresIn: 999999999,
+        })
+
+        cookies().set({
+          name: `${payload.config.cookiePrefix}-token`,
+          value: token,
+          httpOnly: true,
+          secure: true,
+          sameSite: 'Strict',
+          path: '/',
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+        })
+
+        return Response.json(userDocs)
+      },
     },
   ],
 }
